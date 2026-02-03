55f0a2c76a066d779a1e897213ddbb541606180f
Hamid <hamidsaif214@gmail.com>
2026-02-03 17:46:15 +0500

    Refactor password hashing in User model
    Refactor password hashing middleware to remove next() calls.

--- src/models/User.js
+++ src/models/User.js
@@ -39,18 +39,17 @@ const UserSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 // Hash password before saving
-UserSchema.pre('save', async function (next) {
+UserSchema.pre('save', async function () {
   // 1. Only run if password is modified AND exists
   if (!this.isModified('password') || !this.password) {
-    return next(); 
+    return; 
   }
 
   try {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
   } catch (error) {
-    next(error);
+    throw error;
   }
 });
 
